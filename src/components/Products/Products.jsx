92be7fefd28ba1579096f255d4c123e6f6f2d706
Fernando <ferweb.reyna@gmail.com>
2022-10-19 20:21:49 -0300

    products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ const productos = [
     {id:2, nombre:"Gorra Irlandesa", destacado:"infalible ", descripcion:"Gorra años 20'", imagen:"https://static.caphunters.com/11925-large_default/boina-gris-ari-de-goorin-bros.webp", precio:6450, categoria:"indumentaria", stock:8},
     {id:3, nombre:"Boina Peaky Blinders", destacado:"infaltable", descripcion:"Gorra años 20'", imagen:"https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/b/o/boina-goorin-ivy-verde-501041030283903-1.jpg", precio:8050, categoria:"indumentaria", stock:4},
     {id:4, nombre:"Boina SpeakBlinders", destacado:"intocable", descripcion:"Gorra años 20'", imagen:"https://www.goorinbros.com.ar/media/catalog/product/cache/8/thumbnail/9df78eab33525d08d6e5fb8d27136e95/1/0/103-0257-004.jpg", precio:8750, categoria:"indumentaria", stock:14},
-    {id:5, nombre:"Boina EasyBlinders", destacado:"increible", descripcion:"Gorra años 20'", imagen:"https://http2.mlstatic.com/D_NQ_NP_676606-MLA51522507261_092022-O.webp", precio:8050, categoria:"indumentaria", stock:4},
+    {id:5, nombre:"Boina EasyBlinders", destacado:"increible", descripcion:"Gorra años 20'", imagen:"https://http2.mlstatic.com/D_NQ_NP_676606-MLA51522507261_092022-O.webp", precio:9150, categoria:"indumentaria", stock:4},
     {id:6, nombre:"Boina IrishBlind", destacado:"inombrable", descripcion:"Gorra años 20'", imagen:"https://http2.mlstatic.com/D_NQ_NP_984548-MLA50126267751_052022-O.webp", precio:9250, categoria:"indumentaria", stock:7},
     
 ];
