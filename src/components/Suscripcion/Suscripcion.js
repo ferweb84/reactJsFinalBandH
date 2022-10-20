2f205cb5e46801d5a03d7122ec4a08f5e39174dc
Fernando <ferweb.reyna@gmail.com>
2022-10-20 00:27:33 -0300

    cambiandoSusc

--- src/components/Suscripcion/Suscripcion.js
+++ src/components/Suscripcion/Suscripcion.js
@@ -4,13 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
+
 export const Suscripcion = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_xzprr5s', '1zHool9GL-wwujzE_', form.current, '1zHool9GL-wwujzE_')
+    emailjs.sendForm('service_xzprr5s', 'template_8amglno', form.current, '1zHool9GL-wwujzE_')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -20,22 +21,22 @@ export const Suscripcion = () => {
 
   return (
     <form ref={form} onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Nombre y Apellido</Form.Label>
+        <Form.Control placeholder="Ingresa Nombre y Apellido" />   
+      </Form.Group>       
       <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Direccion de Email</Form.Label>
         <Form.Control type="email" placeholder="Ingresa Email" />
         
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingresa Contraseña" />
-        <Form.Text className="text-muted">
-          Nunca reveles tu contraseña.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Escribe tu Mensaje </Form.Label>
+        <Form.Control as="textarea" rows={3} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check type="checkbox" label="Seleccionar para enviar" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Enviar
